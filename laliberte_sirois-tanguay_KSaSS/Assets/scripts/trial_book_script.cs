04b1172810bddda9c0ab3eb40be68b9ba92b83f2
CherriRaspberri <103427209+CherriRaspberri@users.noreply.github.com>
2026-05-12 12:31:03 -0400

    Commit - Unity
    - Finished objects sounds
    - Fixed secret room problems
    - Removed evil entity

--- laliberte_sirois-tanguay_KSaSS/Assets/scripts/trial_book_script.cs
+++ laliberte_sirois-tanguay_KSaSS/Assets/scripts/trial_book_script.cs
@@ -8,19 +8,29 @@ public class trial_book_script : MonoBehaviour
     public Animator targetAnimator;
     public string triggerName;
     
+    [Header("Audio Settings")]
+    public AudioSource audioSource;
+    public AudioClip doorOpenSound;
+
     private bool hasTriggered = false;
 
     void Update()
     {
         //Checks if the angle is very close to the limit
-        //hinge.angle returns the current rotation relative to the start position
         if (!hasTriggered && bookHinge.angle >= bookHinge.limits.max - 1f)
         {
             //Sets trigger for animation
             targetAnimator.SetTrigger(triggerName);
-            //Prevents the animation from firing every frame
+
+            //PLAY SOUND HERE
+            if (audioSource != null && doorOpenSound != null)
+            {
+                audioSource.PlayOneShot(doorOpenSound);
+            }
+
+            //Prevents the animation and sound from firing every frame
             hasTriggered = true; 
-            Debug.Log("Book Opened: Animation Triggered!");
+            Debug.Log("Book Opened: Animation and Sound Triggered!");
         }
     }
 }
