466e147a8687787cd68933fb1f769feb293b430e
Cedricst789 <1937033@cmontmorency.qc.ca>
2026-05-12 11:02:06 -0400

    commit unity
    timer régler
    game over fini

--- laliberte_sirois-tanguay_KSaSS/Assets/scripts/RoomTimer.cs
+++ laliberte_sirois-tanguay_KSaSS/Assets/scripts/RoomTimer.cs
@@ -26,6 +26,7 @@ public float tempsRestant = 300f; //(5 min) Temps en secondes
             {
                 tempsRestant = 0;
                 timerActif = false;
+                auTempsEcoule.Invoke();
             }
         }
     }
